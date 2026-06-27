28ce4210918855e2ab77488fecd04ef47d083130
niteshyadav7 <nitesh0709yadav@gmail.com>
2026-06-27 16:11:34 +0530

    revert: remove BackgroundBeamsWithCollision wrapper from hero template

--- hero-section/app/components/templates/HeroSection.jsx
+++ hero-section/app/components/templates/HeroSection.jsx
@@ -1,12 +1,11 @@
 import Navbar from "../organisms/Navbar";
 import HeroContent from "../organisms/HeroContent";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 
 export default function HeroSection({ navbarProps = {}, heroProps = {} }) {
   return (
-    <BackgroundBeamsWithCollision className="flex-col w-full bg-white dark:bg-neutral-950 transition-colors duration-300">
+    <div className="relative w-full bg-white dark:bg-neutral-950 transition-colors duration-300">
       <Navbar {...navbarProps} />
       <HeroContent {...heroProps} />
-    </BackgroundBeamsWithCollision>
+    </div>
   );
 }
